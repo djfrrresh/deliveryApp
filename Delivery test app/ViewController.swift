0fc4ae671f3db08321d3c11014b91af9231c8872
fakin_kiska <kunavinjenya55@gmail.com>
2023-04-03 15:01:37 +0400

    Initial Commit

---
+++ Delivery test app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Delivery test app
+//
+//  Created by Eugene on 03.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
